d640a2931de3ad458e833180c39ebb60e364e623
Yojhan Arias <zigzagflete@gmail.com>
2025-09-13 15:47:02 -0500

    Implementacion de Swagger

--- grupo6_Ventas/grupo6_Ventas/settings.py
+++ grupo6_Ventas/grupo6_Ventas/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "drf_yasg",
     'apps.ventas',
     "rest_framework",
     "django_filters",
+    
 ]
 
 MIDDLEWARE = [
