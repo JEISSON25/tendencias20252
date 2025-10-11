45b4af1ad6c6e4ac7811c8e1e84d2130114a08ab
LisDk7 <125290355+LisDk7@users.noreply.github.com>
2025-10-11 12:46:28 -0500

    Actualizacion motor BD

--- gestionrecursos/gestionrecursos/settings.py
+++ gestionrecursos/gestionrecursos/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = 'gestionrecursos.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'your_database_name',
+'USER': 'your_username',
+'PASSWORD': 'your_password',
+'HOST': 'localhost',
+'PORT':'‘5432', # default PostgreSQL port
+}
 }
 
 
