afc13e6b181e47f3935d688362f7c7d31b09713c
Juan Jose Tellez Rojas <jtellez@MacBook-Air-de-Juan.local>
2025-10-18 11:55:21 -0500

    fix: db server from env

--- smartreservations/settings.py
+++ smartreservations/settings.py
@@ -123,8 +123,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
