d2da2912798f2a2fcdbe6e97cf11d880fb0cecf6
AnaMorales4 <alt.anamor4@gmail.com>
2025-11-10 19:23:23 -0500

    add requitements

--- gestionrecursos/gestionrecursos/settings.py
+++ gestionrecursos/gestionrecursos/settings.py
@@ -87,31 +87,31 @@ WSGI_APPLICATION = 'gestionrecursos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#         'default': {
-#                'ENGINE': 'django.db.backends.postgresql',
-#                'NAME': 'neondb',
-#                'USER': 'neondb_owner',
-#                'PASSWORD': 'npg_48lNhbeJKXZi',
-#                'HOST': 'ep-patient-fire-ahja6094.c-3.us-east-1.aws.neon.tech',
-#                'PORT': '5432',
-#                'TEST': {
-#                     'NAME': f'test_neondb_{uuid.uuid4().hex[:6]}',
-#                     'SERIALIZE': False
-#                 },
-#                'OPTIONS': {
-#                      'sslmode': 'require',
-#                 },
-#                 'CONN_MAX_AGE': 0,
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+        'default': {
+               'ENGINE': 'django.db.backends.postgresql',
+               'NAME': 'neondb',
+               'USER': 'neondb_owner',
+               'PASSWORD': 'npg_48lNhbeJKXZi',
+               'HOST': 'ep-patient-fire-ahja6094.c-3.us-east-1.aws.neon.tech',
+               'PORT': '5432',
+               'TEST': {
+                    'NAME': f'test_neondb_{uuid.uuid4().hex[:6]}',
+                    'SERIALIZE': False
+                },
+               'OPTIONS': {
+                     'sslmode': 'require',
+                },
+                'CONN_MAX_AGE': 0,
+    }
+}
 
 
 REST_FRAMEWORK = {
--- gestionrecursos/gestionrecursos/urls.py
+++ gestionrecursos/gestionrecursos/urls.py
@@ -38,7 +38,7 @@ router.register(r'recursos', RecursosViewSet)
 
 
 urlpatterns = [
-    path('api/register/', RegisterView.as_view(), name='api-register'),  # ← aquí
+    # front
     path('', initial_view, name='initial'),
     path('login/', login_view, name='login'),
     path('home/', home_view, name='home'),
@@ -47,16 +47,20 @@ urlpatterns = [
     path('recursos/', recursos_view, name='recursos'),
     path('reservas/', reservas_view, name='reservas'),
     path('admin/', admin.site.urls),
+    #apis
     path('api/', include(router.urls)),
+    path('api/register/', RegisterView.as_view(), name='api-register'),  # ← aquí
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls')),
+    #Exportar
     path('export/recursos/pdf', export_recursos_to_pdf, name='export_recursos_to_pdf'),
     path('export/recursos/json', export_recursos_to_json, name='export_recursos_to_json'),
     path('export/reservas/json', export_reservas_to_json, name='export_reservas_to_json'),
     path('export/reservas/pdf', export_reservas_to_pdf, name='export_reservas_to_pdf'),
+    #documentacion
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api-auth/', include('rest_framework.urls')),
 ]
 
