68b014f85ac76304626c123f67492c0d3a2efbd5
INGJEISSON <jeisim18@gmail.com>
2025-11-15 11:20:25 -0500

    fix

--- gestionrecursos/apps/roles/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Roles
-
-class RolesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Roles
-        fields = "__all__"
